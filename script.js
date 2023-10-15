63aa1080aa0ee0f22ec222b16c308f00616f9588
Sara <sarakerr222@gmail.com>
2023-10-15 17:46:17 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const todoList = [
+  {
+    name: "",
+    dueDate: "",
+  },
+];
+
+function renderTodoList() {
+  let todoListHTML = "";
+
+  for (let i = 1; i < todoList.length; i++) {
+    const name = todoList[i].name;
+    const dueDate = todoList[i].dueDate;
+
+    const html = `
+    
+    <div> ${name}</div>
+     <div>${dueDate}</div>
+     <button onclick ="
+     todoList.splice(${i},1);
+     renderTodoList()
+     " class ="delete-btn">Delete</button>
+     `;
+
+    todoListHTML += html;
+  }
+
+  document.querySelector(".js-todo-list").innerHTML = todoListHTML;
+}
+
+function addTodo() {
+  const inputElement = document.querySelector(".input-task");
+  const name = inputElement.value;
+
+  const dateInputElement = document.querySelector(".js-due-date-input");
+  const dueDate = dateInputElement.value;
+
+  todoList.push({
+    name: name,
+    dueDate: dueDate,
+  });
+
+  inputElement.value = "";
+
+  renderTodoList();
+}
